489fbbb5b3c376288ceb32a5142bb95ab2063268
Maxim Volkov <mx_ruz_net@hotmail.com>
2021-01-30 17:13:04 +0300

    continue capture behavor

---
+++ Aweton.Mxw.DocxGenerator.Test/DocxGeneratorGenerateTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Aweton.Mxw.DocxGenerator.Test
+{
+  [TestClass]
+  public class DocxGeneratorGenerateTests
+  {
+    [TestMethod]
+    public void GenerateFilesThrowsIfNoDataLoaded()
+    {
+      var dg = new DocumentGenerator.DocxGenerator("", 0);
+      dg.GenerateFiles(null);
+    }
+  }
+}
---
+++ Aweton.Mxw.DocxGenerator.Test/DocxGeneratorGetFilesAsTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Aweton.Mxw.DocxGenerator.Test
+{
+  [TestClass]
+  public class DocxGeneratorGetFilesAsTests
+  {
+    [TestMethod]
+    public void GetFilesAsPathsThrowsIfCalledBeforeGenerated()
+    {
+      var dg = new DocumentGenerator.DocxGenerator($"{Guid.NewGuid()}", 0);
+      _ = Assert.ThrowsException<NullReferenceException>(() => dg.GetFilesAsPaths(string.Empty));
+    }
+    [TestMethod]
+    public void GetFilesAsBytesReturnsNullIfCalledBeforGenerated()
+    {
+      var dg = new DocumentGenerator.DocxGenerator($"{Guid.NewGuid()}", 0);
+      Assert.IsNull(dg.GetFilesAsBytes());
+    }
+  }
+}
